4d959ad80973843d84bb82e54705e75de0ca0e7d
Carlos Loya <cloyajim@gmail.com>
2023-03-07 11:16:58 -0600

    clase MainRepository

---
+++ app/src/main/java/com/example/detectorsismos/MainRepository.kt
@@ -0,0 +1,38 @@
+package com.example.detectorsismos
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class MainRepository {
+    suspend fun fetchEarthquakes(): MutableList<Earthquake> {
+        return withContext(Dispatchers.IO) {
+            val eqJsonResponse = service.getLastHourEarthquake()
+            val eqList = parceEqResult(eqJsonResponse)
+
+            eqList
+        }
+    }
+
+    private fun parceEqResult(eqJsonResponse: EqJsonResponse): MutableList<Earthquake> {
+        val eqList = mutableListOf<Earthquake>()
+        val featureList = eqJsonResponse.features
+
+        for(features in featureList){
+            val properties = features.properties
+            val id = features.id
+
+            val mag = properties.mag
+            val place = properties.place
+            val time = properties.time
+
+            val geometry = features.geometry
+            val latitude = geometry.latitude
+            val longitude = geometry.longitude
+
+            eqList.add(Earthquake(id,place,mag,time,longitude,latitude))
+
+        }
+
+        return eqList
+    }
+}
--- app/src/main/java/com/example/detectorsismos/MainViewModel.kt
+++ app/src/main/java/com/example/detectorsismos/MainViewModel.kt
@@ -8,6 +8,8 @@ import kotlinx.coroutines.*
 
 class MainViewModel: ViewModel() {
 
+    private val mainRepository = MainRepository()
+
     private var _eqList: MutableLiveData<MutableList<Earthquake>> =
         MutableLiveData<MutableList<Earthquake>>()
     val eqList: LiveData<MutableList<Earthquake>>
@@ -16,41 +18,11 @@ class MainViewModel: ViewModel() {
     init {
         viewModelScope.launch {
 
-            _eqList.value = fetchEarthquakes()
+            _eqList.value = mainRepository.fetchEarthquakes()
         }
     }
 
-    private suspend fun fetchEarthquakes(): MutableList<Earthquake> {
-        return withContext(Dispatchers.IO) {
-            val eqJsonResponse = service.getLastHourEarthquake()
-            val eqList = parceEqResult(eqJsonResponse)
 
-            eqList
-        }
-    }
-
-    private fun parceEqResult(eqJsonResponse: EqJsonResponse): MutableList<Earthquake> {
-        val eqList = mutableListOf<Earthquake>()
-        val featureList = eqJsonResponse.features
-
-        for(features in featureList){
-            val properties = features.properties
-            val id = features.id
-
-            val mag = properties.mag
-            val place = properties.place
-            val time = properties.time
-
-            val geometry = features.geometry
-            val latitude = geometry.latitude
-            val longitude = geometry.longitude
-
-            eqList.add(Earthquake(id,place,mag,time,longitude,latitude))
-
-        }
-
-        return eqList
-    }
 }
 
 
